bf5b40feabec7a8c0541e4693fa4bb6a236245fb
dak2 <dawt2h@gmail.com>
2023-01-20 09:17:16 +0900

    add patern matching

--- projects/guessing_game/src/main.rs
+++ projects/guessing_game/src/main.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
   println!("Guess the number!");
+  let secret_number = rand::thread_rng().gen_range(1..101);
+
+  println!("The secret number is: {}", secret_number);    //秘密の数字は次の通り: {}
   println!("Please input your guess.");
 
   let mut guess = String::new();
@@ -10,5 +15,13 @@ fn main() {
     .read_line(&mut guess)
     .expect("Failed to read line");
 
+  let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
   println!("You guessed: {}", guess);
+
+  match guess.cmp(&secret_number) {
+    Ordering::Less => println!("Too small!"),       //小さすぎ！
+    Ordering::Greater => println!("Too big!"),      //大きすぎ！
+    Ordering::Equal => println!("You win!"),        //やったね！
+  }
 }
